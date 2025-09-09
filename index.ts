32c21b0d28567199355361b352c2ecc57235632d
Vítor Marques <dleggdraw@gmail.com>
2025-09-09 09:25:54 -0300

    feitos alguns ajustes no menu, nas functions, e break nas options

--- index.ts
+++ index.ts
@@ -25,12 +25,16 @@ while(true){
             break;
         case 2:
             frear(carro)
+            break
         case 3:
             subirMarcha(carro)
+            break
         case 4:
             descerMarcha(carro)
+            break
         case 5:
             imprimirDados(carro)
+            break
         default:
             break;
     }
@@ -45,28 +49,31 @@ function acelerar(veiculo: Veiculo): void{
 }}
 
 function frear(veiculo: Veiculo): void{
-    if(veiculo.marchaAtual != 0){
-    veiculo.velocidade += veiculo.potencia* -0.1;
+    if(veiculo.velocidade > 0 && veiculo.marchaAtual != 0){
+    veiculo.velocidade -= veiculo.potencia * 0.1;
     console.log(veiculo.velocidade);
 }}
 function subirMarcha(veiculo: Veiculo): void{
-    if(veiculo.marchaAtual == 0){
-        acelerar(veiculo)
+    if(veiculo.marchaAtual - veiculo.numeroMarchas){
+        veiculo.marchaAtual++
+        console.log("Subindo marcha do veículo!")
     }
 }
 function descerMarcha(veiculo: Veiculo):void{
-    if(veiculo.marchaAtual>0){
+    if(veiculo.marchaAtual - veiculo.numeroMarchas){
         veiculo.marchaAtual--
-        
+        console.log("O veículo ddesceu a marcha.")
     }
 }
 function imprimirDados(veiculo: Veiculo):void{
-    console.log("Dados do veículo: ")
+    console.log("===== Dados do veículo =====")
     console.log("-".repeat(30))
     console.log(`Cor do veículo: ${veiculo.cor}`)
     console.log(`Marca do veículo: ${veiculo.marca}`)
     console.log(`Modelo do veículo: ${veiculo.modelo}`)
     console.log(`Potência do veículo${veiculo.potencia}`)
+    console.log("--------------------------------------")
+    console.log("===== Dados impressos =====")
 }
 
 function criaVeiculo(): Veiculo{
